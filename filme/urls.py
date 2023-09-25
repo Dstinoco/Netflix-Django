b3b4c7bbf492699bedbdb4e98fc5c2c7a1da9f72
Douglas Tinoco <douglas.bart@hotmail.com>
2023-09-24 21:42:06 -0300

    urls, views, statics

--- filme/urls.py
+++ filme/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from .views import homepage
 
-"""urlpatterns = [
-    path
-]"""
+urlpatterns = [
+    path('', homepage, name='homepage')
+]
